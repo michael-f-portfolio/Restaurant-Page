48e98c1999d6407cd0d299e1b158c12fc7cdfc60
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2023-12-28 09:04:47 -0700

    removed unused declaration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
@@ -10,7 +9,6 @@ module.exports = merge(common, {
         path: path.resolve(__dirname, "dist"),
         // clean: true,
     },
-
     devtool: "inline-source-map",
     devServer: {
         static: "./dist",
